083ef03cfcf3d0f2ebeebd85de8e5d577f296a8b
rhyde07 <155705433+rhyde07@users.noreply.github.com>
2026-04-03 22:54:16 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	
+	while(n--) {
+		int a, b;
+		cin >> a >> b;
+		
+		cout << "yes\n";
+	}
+	
+	return 0;
+}
